91697fba8ab839d5670a63a2b7a2405211b9e23d
ssdy_dev <ssdy_dev@outlook.com>
2021-11-15 11:11:16 +0800

    remove comment in build files

--- do32.sh
+++ do32.sh
@@ -1,5 +1,4 @@
 make clean
 ./configure --arch=x86_32 --enable-x86asm --enable-shared --enable-static --enable-dxva2 --enable-avformat --enable-avcodec --enable-avutil --enable-swscale --enable-swresample --enable-avfilter  --prefix=/usr/local/ffmpeg4_32
-#./configure --arch=x86_32 --enable-x86asm --enable-shared --enable-static --enable-dxva2 --enable-avformat --enable-avcodec --enable-avutil --enable-swscale --enable-swresample --enable-avfilter --enable-gpl --enable-nonfree --enable-postproc  --prefix=/usr/local/ffmpeg4_32
 make -j8
 make install
--- do64.sh
+++ do64.sh
@@ -1,5 +1,4 @@
 make clean
-#./configure --arch=x86_64 --enable-x86asm --enable-shared --disable-static --prefix=/usr/local/ffmpeg4_64
 ./configure --arch=x86_64 --enable-x86asm --enable-shared --enable-static --enable-dxva2 --enable-avformat --enable-avcodec --enable-avutil --enable-swscale --enable-swresample --enable-avfilter  --prefix=/usr/local/ffmpeg4_64
 make -j8
 make install
